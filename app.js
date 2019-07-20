d941c8e621c3dbefe792a3c29d4f6eb1a1b47a67
sharmamayank94 <sharmamayank941@gmail.com>
2019-07-20 20:06:14 +0530

    port chnage

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get('/get', (req, res)=>{
 	console.log('hey i am get');
 	res.send(htmlcode);
 })
-app.listen(3000, (a, b)=>{
+app.listen('http://env-5613509.mj.milesweb.cloud/', (a, b)=>{
 	console.log('server is running');
 })
